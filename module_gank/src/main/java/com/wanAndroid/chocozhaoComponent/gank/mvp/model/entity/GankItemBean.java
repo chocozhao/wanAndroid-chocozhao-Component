ca9031b8db8a3c596d76e4a9ec00ca1facf82d39
chocozhao <1027313530@qq.com>
2020-12-08 11:39:23 +0800

    Initial commit

---
+++ module_gank/src/main/java/com/wanAndroid/chocozhaoComponent/gank/mvp/model/entity/GankItemBean.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2018 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wanAndroid.chocozhaoComponent.gank.mvp.model.entity;
+
+import java.util.List;
+
+/**
+ * ================================================
+ * Created by JessYan on 08/05/2016 12:05
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class GankItemBean {
+    /**
+     * {
+     * "_id": "5e8c80ae2bce50b3ceaa80f0",
+     * "author": "鸢媛",
+     * "category": "Girl",
+     * "createdAt": "2020-04-11 08:00:00",
+     * "desc": "我没那么坚强，只是习惯了什么事都自己扛。​",
+     * "images": [
+     *      "http://gank.io/images/1c5cebd307fd49eaa75b368b11118b61"
+     * ],
+     * "likeCounts": 0,
+     * "publishedAt": "2020-04-11 08:00:00",
+     * "stars": 1,
+     * "title": "第52期",
+     * "type": "Girl",
+     * "url": "http://gank.io/images/1c5cebd307fd49eaa75b368b11118b61",
+     * "views": 258
+     * }
+     */
+    private String id;
+    private String author;
+    private String category;
+    private String createdAt;
+    private String desc;
+    private List<String> images = null;
+    private int likeCounts;
+    private String publishedAt;
+    private int stars;
+    private String title;
+    private String type;
+    private String url;
+    private int views;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public int getLikeCounts() {
+        return likeCounts;
+    }
+
+    public void setLikeCounts(int likeCounts) {
+        this.likeCounts = likeCounts;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
